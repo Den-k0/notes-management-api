88692c788406be95ce13c6bf2f5c32174b16968d
Den_k0 <denys.kotovskiy@gmail.com>
2025-03-23 15:46:26 +0200

    implement schemas for notes

---
+++ src/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
+
+
+class NoteBaseSchema(BaseModel):
+    title: str = Field(max_length=100)
+    content: str
+
+
+class NoteCreateRequestSchema(NoteBaseSchema):
+    pass
+
+
+class NoteResponseSchema(BaseModel):
+    id: int
+    title: str
+    content: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class NoteDetailResponseSchema(NoteResponseSchema):
+    summary: str | None = None
+    created_at: datetime
+    version: int
+    previous_version_id: int | None = None
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class NoteUpdateRequestSchema(NoteBaseSchema):
+    pass
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
